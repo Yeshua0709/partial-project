58157604978bdeb9f383396229c65eb77f580ef0
Yeshua0709 <abrenica.y@gmail.com>
2023-03-21 00:28:14 +0800

    DB Added

---
+++ SneakerDB/dbo/Tables/Item.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Item]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [name] NVARCHAR(50) NOT NULL, 
+    [brand] NVARCHAR(50) NOT NULL, 
+    [price] INT NOT NULL, 
+    [quantity] INT NOT NULL
+)
