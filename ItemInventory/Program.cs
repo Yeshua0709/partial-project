9a37a70bc9e83f5f148fcf87feb9d11332124170
Yeshua0709 <abrenica.y@gmail.com>
2023-03-21 00:26:32 +0800

    Web API Added

---
+++ ItemInventory/Program.cs
@@ -0,0 +1,58 @@
+using ItemInventory.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ItemInventory
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddCors(options => options.AddPolicy(name: "ItemOrigins",
+    policy =>
+    {
+        policy.WithOrigins("https://localhost:44480").AllowAnyMethod().AllowAnyHeader();
+    }));
+
+            builder.Services.AddDbContext<ItemDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("ItemDbContext")));
+
+
+         
+
+      
+            var app = builder.Build();
+
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseCors("ItemOrigins");
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+           
+            app.Run();
+
+           
+        }
+
+       
+    }
+}
